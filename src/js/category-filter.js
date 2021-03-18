1e9b7d9ca05ecd3fdb68bd1f8cd94f78f4335817
Andrey <artist_hire@ukr.net>
2021-03-18 09:21:22 +0200

    Изменен скрипт обработки фильтра категорий

--- src/js/category-filter.js
+++ src/js/category-filter.js
@@ -1,14 +1,23 @@
 const btnClass = 'js-category-filter-btn';
+const hiddenInputClass = 'js-category-filter-input';
 const activeClass = 'active';
 
-const categoryFilterBtns = document.querySelectorAll(`.${btnClass}`);
 const btnContainer = document.querySelector('.js-category-filter-container');
+const categoryFilterBtns = btnContainer.querySelectorAll(`.${btnClass}`);
+const hiddenInput = document.querySelector(`.${hiddenInputClass}`);
 
 btnContainer.addEventListener('click', (evt) => {
   if (evt.target.closest(`.${btnClass}`)) {
     evt.preventDefault();
 
-    categoryFilterBtns.forEach((btn) => btn.classList.remove(activeClass));
+    categoryFilterBtns.forEach((btn) => {
+      btn.classList.remove(activeClass);
+      btn.removeAttribute('tabindex');
+    });
+
     evt.target.classList.add(activeClass);
+    evt.target.setAttribute('tabindex', '-1');
+
+    hiddenInput.value = evt.target.dataset.productId;
   }
 });
